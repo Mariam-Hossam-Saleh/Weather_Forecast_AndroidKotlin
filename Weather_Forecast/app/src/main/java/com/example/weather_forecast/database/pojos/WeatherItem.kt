fafa4a05e43a2ab0cdf79a95c01aa8fe1a98ad07
Mariam <mariam.hossam.keshk@gmail.com>
2025-05-19 17:55:40 +0300

    Feature: Added Pojos of the API respose

---
+++ Weather_Forecast/app/src/main/java/com/example/weather_forecast/database/pojos/WeatherItem.kt
@@ -0,0 +1,4 @@
+package com.example.weather_forecast.database.pojos
+
+class WeatherItem {
+}
---
+++ Weather_Forecast/app/src/main/java/com/example/weather_forecast/network/RetrofitHelper.kt
@@ -0,0 +1,13 @@
+package com.example.weather_forecast.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitHelper{
+    private const val BASE_URL = "https://dummyjson.com/"
+    private val retrofitInstance = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    val service: ProductsService = retrofitInstance.create(ProductsService::class.java)
+}
